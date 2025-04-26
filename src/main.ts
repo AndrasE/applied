fd3b245b8a9246005732ba050f52f50eab8970d9
Andras E <rohadtsajt@gmail.com>
2025-04-26 23:20:33 +0100

    sort vite ts clash not having @ imports
    vite:
    resolve: {
        alias: {
          "@": path.resolve(__dirname, "src"),
        },
    
    tsconfig
    "paths": {
          "@/*": ["src/*"]
        }
      },
    
    shims-vue.d.ts added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "../router/index"; // Import the router instance
+import router from "../router/index";
 import "./style.css";
 
 const app = createApp(App);
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
-import vueDevTools from "vite-plugin-vue-devtools"; // Import the plugin
+import vueDevTools from "vite-plugin-vue-devtools";
+import path from "path";
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), vueDevTools()], // Add the plugin here
+  plugins: [vue(), tailwindcss(), vueDevTools()],
   server: {
     port: 3000,
     open: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
 });
