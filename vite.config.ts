0e51721672456cbb38f21d88cb0e18728a8ba043
AndrasE <rohadtsajt@gmail.com>
2025-04-26 21:52:18 +0100

    router and vue devtools install and some refactoring

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import vueDevTools from "vite-plugin-vue-devtools"; // Import the plugin
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), vueDevTools()], // Add the plugin here
   server: {
     port: 3000,
     open: true,
