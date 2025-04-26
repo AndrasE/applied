0c0aed55ef6a2e074b53bba2a098e7271958d9e3
Andras E <rohadtsajt@gmail.com>
2025-04-26 12:25:02 +0100

    initial setup vite+vue tailwind and typescript

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold">{{ msg }}</h1>
+  <div class="mt-4 bg-gray-100 p-4">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="count++">
+      count is: {{ count }}
+    </button>
+  </div>
+</template>
