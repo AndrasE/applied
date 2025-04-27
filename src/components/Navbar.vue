1de93876c77b37235213b5f8081350e9f094f6bb
Andras E <rohadtsajt@gmail.com>
2025-04-27 11:15:04 +0100

    navbar start

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,27 @@
 <script setup lang="ts">
+import { RouterLink, useRoute } from 'vue-router';
+
+// const isActiveLink = (routePath: string) => {
+//     const route = useRoute();
+//     return route.path === routePath;
+// };
+
 </script>
+
 <template>
-    navasasas
+    <nav
+        class="fixed bottom-0 w-full z-50 h-auto sm:top-0 sm:left-0 sm:h-full sm:w-auto dark:bg-gray-950 dark:shadow-gray-300">
+        <div
+            class="flex flex-row gap-3 justify-center items-center p-2 h-full sm:flex-col sm:justify-center sm:items-center">
+            <RouterLink to="/" class="text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg">
+                Home
+            </RouterLink>
+            <RouterLink to="/about" class=" text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg">
+                About
+            </RouterLink>
+            <RouterLink to="/services" class=" text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg">
+                Services
+            </RouterLink>
+        </div>
+    </nav>
 </template>
