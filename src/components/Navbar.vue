671eac92e39b7e5589059b40ff3c63d539463158
Andras E <rohadtsajt@gmail.com>
2025-04-28 07:54:08 +0100

    navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,24 +15,30 @@ import { Icon } from "@iconify/vue";
         class="fixed bottom-0 w-full z-50 h-auto sm:top-0 sm:left-0 sm:h-full sm:w-auto dark:bg-gray-950 dark:shadow-gray-300">
         <div
             class="flex flex-row justify-evenly items-center p-2 gap-2 h-full  sm:gap-5 sm:flex-col sm:justify-center sm:items-center">
+            <span
+                class="hidden sm:flex sm:flex-col justify-center items-center w-full  text-gray-900 dark:text-gray-300 text-lg">
+                <Icon icon="bxl:visual-studio" class="text-4xl rotate-90" />
+
+
+            </span>
             <RouterLink to="/"
                 class="flex flex-col justify-center items-center w-full  text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg">
-                <Icon icon="ci:house-01" class="text-4xl" />
+                <Icon icon="ci:house-01" class="text-2xl sm:text-4xl " />
                 Home
             </RouterLink>
-            <RouterLink to="/jobs"
+            <RouterLink to=" /jobs"
                 class="flex flex-col justify-center items-center w-full  text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg">
-                <Icon icon="ci:handbag" class="text-4xl" />
+                <Icon icon="ci:handbag" class="text-2xl sm:text-4xl" />
                 Jobs
             </RouterLink>
             <RouterLink to="/addjob"
                 class="flex flex-col justify-center items-center w-full  text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg">
-                <Icon icon="ci:file-add" class="text-4xl" />
+                <Icon icon="ci:file-add" class="text-2xl sm:text-4xl" />
                 Add
             </RouterLink>
-            <RouterLink to="/about"
+            <RouterLink to=" /about"
                 class="flex flex-col justify-center items-center w-full  text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg">
-                <Icon icon="ci:text-align-left" class="text-4xl" />
+                <Icon icon="ci:text-align-left" class="text-2xl sm:text-4xl" />
                 About
             </RouterLink>
         </div>
