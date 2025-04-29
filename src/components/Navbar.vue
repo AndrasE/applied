217f2aa8ff211479a8d6d39a0bda7707979b0c87
Andras E <rohadtsajt@gmail.com>
2025-04-29 07:33:14 +0100

    design idea 2 nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,39 +13,46 @@ import { Icon } from "@iconify/vue";
 <template>
     <nav
         class="fixed bottom-0 w-full z-50 h-auto sm:top-0 sm:left-0 sm:h-full sm:w-auto dark:bg-gray-950 dark:shadow-gray-300">
+
         <div
-            class="flex flex-row justify-evenly items-center p-2 gap-2 sm:gap-5 sm:flex-col sm:justify-start sm:items-center sm:flex-1">
+            class="flex flex-row justify-evenly items-center gap-2 sm:flex-col sm:justify-between sm:items-center sm:h-full sm:w-auto">
+            <div class="hidden sm:flex sm:flex-col sm:items-start">
+                <RouterLink to="/"
+                    class=" items-center w-full text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg">
+                    <Icon icon="material-symbols-light:font-download-outline-rounded" class="text-6xl" />
+                </RouterLink>
+            </div>
+            <div class="flex flex-row sm:flex-col items-center justify-center flex-grow p-2 sm:p-0 gap-2 sm:gap-y-4 ">
+                <RouterLink to="/"
+                    class="flex flex-col justify-center items-center w-full text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg transition-transform duration-300 ease-in-out sm:hover:scale-103">
+                    <Icon icon="ci:house-01" class="text-2xl sm:text-4xl" />
+                    Home
+                </RouterLink>
 
-            <!-- Logo -->
-            <RouterLink to="/"
-                class="hidden sm:flex sm:flex-col sm:mt-4 items-center w-full text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg">
-                <Icon icon="material-symbols-light:font-download-outline-rounded" class="text-6xl" />
-            </RouterLink>
+                <RouterLink to="/jobs"
+                    class="flex flex-col justify-center items-center w-full text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg transition-transform duration-300 ease-in-out sm:hover:scale-103">
+                    <Icon icon="ci:handbag" class="text-2xl sm:text-4xl" />
+                    Jobs
+                </RouterLink>
 
-            <!-- Nav RouterLinks -->
-            <RouterLink to="/"
-                class="flex flex-col justify-center items-center w-full text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg transition-transform duration-300 ease-in-out sm:hover:scale-103">
-                <Icon icon="ci:house-01" class="text-2xl sm:text-4xl" />
-                Home
-            </RouterLink>
+                <RouterLink to="/addjob"
+                    class="flex flex-col justify-center items-center w-full text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg transition-transform duration-300 ease-in-out sm:hover:scale-103">
+                    <Icon icon="ci:file-add" class="text-2xl sm:text-4xl" />
+                    Add
+                </RouterLink>
 
-            <RouterLink to="/jobs"
-                class="flex flex-col justify-center items-center w-full text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg transition-transform duration-300 ease-in-out sm:hover:scale-103">
-                <Icon icon="ci:handbag" class="text-2xl sm:text-4xl" />
-                Jobs
-            </RouterLink>
-
-            <RouterLink to="/addjob"
-                class="flex flex-col justify-center items-center w-full text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg transition-transform duration-300 ease-in-out sm:hover:scale-103">
-                <Icon icon="ci:file-add" class="text-2xl sm:text-4xl" />
-                Add
-            </RouterLink>
-
-            <RouterLink to="/about"
-                class="flex flex-col justify-center items-center w-full text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg transition-transform duration-300 ease-in-out sm:hover:scale-103">
-                <Icon icon="ci:text-align-left" class="text-2xl sm:text-4xl" />
-                About
-            </RouterLink>
+                <RouterLink to="/about"
+                    class="flex flex-col justify-center items-center w-full text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg transition-transform duration-300 ease-in-out sm:hover:scale-103">
+                    <Icon icon="ci:text-align-left" class="text-2xl sm:text-4xl" />
+                    About
+                </RouterLink>
+            </div>
+            <div class="hidden sm:flex sm:flex-col sm:items-end">
+                <RouterLink to="/"
+                    class=" items-center w-full text-gray-900 dark:text-gray-300 dark:hover:text-white text-lg">
+                    <Icon icon="material-symbols-light:brightness-6" class="text-6xl" />
+                </RouterLink>
+            </div>
         </div>
     </nav>
 </template>
