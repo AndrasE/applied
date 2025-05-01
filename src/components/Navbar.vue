c5d7ac9eb34d56897656e462090aace5588922c9
Andras E <rohadtsajt@gmail.com>
2025-05-01 07:29:05 +0100

    nav restyle

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,33 +1,40 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
 import { Icon } from "@iconify/vue";
-
-
 // const isActiveLink = (routePath: string) => {
 //     const route = useRoute();
 //     return route.path === routePath;
 // };
-
 </script>
 
 <template>
     <nav
-        class="fixed bottom-0 w-full z-50 h-auto sm:top-0 sm:left-0 sm:h-full sm:w-auto border-t-1 p-2 sm:border-r-1 border-gray-950/5 dark:border-white/10">
+        class="fixed bottom-0 w-full z-50 h-auto sm:top-0 sm:left-0 sm:h-full sm:w-auto border-t-1 p-2 sm:border-r-1 border-subtle">
         <div
             class="flex flex-row justify-evenly items-center gap-2 sm:flex-col sm:justify-between sm:items-center sm:h-full sm:w-auto">
+
             <!-- logo -->
             <div class="hidden sm:flex sm:flex-col sm:items-start">
                 <RouterLink to="/" class="w-full text-hover 3xl">
                     <Icon icon="material-symbols-light:font-download-outline-rounded" class="text-6xl" />
                 </RouterLink>
+
+                <!-- divider line -->
                 <div class="block w-full mt-2 sm:items-end">
-                    <hr class="block w-full border-gray-950/5 dark:border-white/10" />
+                    <div class="w-full h-px bg-slate-300 dark:bg-slate-600"></div>
+                    <p class="text-tiny text-gray-500 dark:text-gray-300 text-center">logo</p>
                 </div>
             </div>
 
             <!-- nav links -->
-            <div
-                class="flex flex-row sm:flex-col items-center justify-center flex-grow p-2 sm:p-0 gap-2 sm:gap-y-4  . border-gray-950/5 dark:border-white/10">
+            <div class="flex flex-row sm:flex-col w-full items-center justify-center flex-grow">
+
+                <!-- divider line -->
+                <div class="block w-full">
+                    <p class="text-tiny text-gray-500 dark:text-gray-300 text-center">nav</p>
+                    <div class="w-full h-px bg-slate-300 dark:bg-slate-600"></div>
+                </div>
+
                 <RouterLink to="/" class="nav-link text-hover">
                     <Icon icon="ci:house-01" class="text-2xl sm:text-3xl" />
                     Home
@@ -47,15 +54,20 @@ import { Icon } from "@iconify/vue";
                     <Icon icon="ci:text-align-left" class="text-2xl sm:text-3xl" />
                     About
                 </RouterLink>
+
+                <!-- divider line -->
+                <div class="block w-full sm:items-end">
+                    <div class="w-full h-px bg-slate-300 dark:bg-slate-600"></div>
+                    <p class="text-tiny text-gray-500 dark:text-gray-300 text-center">links</p>
+                </div>
             </div>
-
-
 
             <!-- theme toggle -->
             <div class="hidden w-full sm:flex sm:flex-col sm:items-end text-center">
-                <div class="block w-full mb-2 sm:items-end">
-                    <p class="text-sky-400/60 text-tiny">theme</p>
-                    <hr class="flex w-full border-gray-950/5 dark:border-white/10" />
+                <!-- divider line -->
+                <div class="block w-full py-2 sm:items-end">
+                    <p class="text-tiny text-gray-500 dark:text-gray-300">theme</p>
+                    <div class="w-full h-px bg-slate-300 dark:bg-slate-600"></div>
                 </div>
                 <RouterLink to="/" class="flex-center text-tiny text-hover w-full 3xl">
                     <Icon icon="material-symbols-light:brightness-6" class="text-3xl" />
