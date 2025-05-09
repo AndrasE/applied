25e14a24a981c1f96165af927b14ecb677180fdf
AndrasE <rohadtsajt@gmail.com>
2025-05-09 10:06:38 +0100

    styling buffer part

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,20 +11,20 @@ const count = ref(0);
 <template>
   <div
     class="grid min-h-screen grid-cols-1 md:grid-cols-[auto_auto_1fr_auto_auto] lg:grid-cols-[1fr_auto_auto_minmax(0,theme(maxWidth.6xl))_auto_auto_1fr]">
-    <div class="hidden lg:flex justify-center text-tiny text-upright">buffer1</div>
+    <div class="hidden lg:flex justify-center items-start text-tiny text-upright">buffer</div>
     <div
       class="border-r border-solid border-color hidden md:block w-14 bg-[image:repeating-linear-gradient(315deg,var(--pattern-color-light)_0,var(--pattern-color-light)_1px,transparent_0,transparent_50%)] dark:bg-[image:repeating-linear-gradient(315deg,var(--pattern-color-dark)_0,var(--pattern-color-dark)_1px,transparent_0,transparent_50%)] bg-[size:10px_10px] bg-fixed">
     </div>
-    <span class="hidden md:flex flex-center text-tiny text-upright">image:repeating</span>
+    <span class="hidden md:flex md:justify-center text-tiny text-upright">image:repeating</span>
     <main class="bg-black-900 w-full text-green-700 border-y-1 border-solid border-color">
       <h1 class="text-3xl font-bold">Hello world!</h1>
       <p>This is the main content area.</p>
       <p>It will be in the middle column on larger screens.</p>
     </main>
-    <span class="hidden md:flex flex-center text-tiny text-upright">linear-gradient</span>
+    <span class="hidden md:flex md:justify-center text-tiny text-upright">linear-gradient</span>
     <div
       class="border-l border-solid border-color hidden md:block w-14 bg-[image:repeating-linear-gradient(315deg,var(--pattern-color-light)_0,var(--pattern-color-light)_1px,transparent_0,transparent_50%)] dark:bg-[image:repeating-linear-gradient(315deg,var(--pattern-color-dark)_0,var(--pattern-color-dark)_1px,transparent_0,transparent_50%)] bg-[size:10px_10px] bg-fixed">
     </div>
-    <div class="hidden lg:flex justify-center text-tiny text-upright">buffer2</div>
+    <div class="hidden lg:flex justify-center items-end text-tiny text-upright">buffer</div>
   </div>
 </template>
