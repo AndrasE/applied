9581963a787f2e696509396360859561484169cf
Andras E <rohadtsajt@gmail.com>
2025-05-09 06:19:33 +0100

    pattern styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
-  <div class="grid min-h-screen grid-cols-1 md:grid-cols-[auto_1fr_auto]
-    lg:grid-cols-[1fr_auto_minmax(0,theme(maxWidth.6xl))_auto_1fr] text-green-700">
-    <div class="  hidden lg:block"></div>
-    <div class="border-x border-solid border-color hidden md:mx-3 md:block w-14"></div>
-    <main class="bg-black-900 w-full ">
+  <div
+    class="grid min-h-screen grid-cols-1 md:grid-cols-[auto_1fr_auto] lg:grid-cols-[1fr_auto_minmax(0,theme(maxWidth.6xl))_auto_1fr]">
+    <div class="hidden lg:block"></div>
+    <div
+      class="border-r border-solid border-color hidden md:block w-14 bg-[image:repeating-linear-gradient(315deg,var(--pattern-color-light)_0,var(--pattern-color-light)_1px,transparent_0,transparent_50%)] dark:bg-[image:repeating-linear-gradient(315deg,var(--pattern-color-dark)_0,var(--pattern-color-dark)_1px,transparent_0,transparent_50%)] bg-[size:10px_10px] bg-fixed">
+    </div>
+    <main class="bg-black-900 w-full text-green-700 border-y-1 border-solid border-color">
+      <h1 class="text-3xl font-bold">Hello world!</h1>
       <p>This is the main content area.</p>
       <p>It will be in the middle column on larger screens.</p>
     </main>
     <div
-      class="border-x border-solid border-color hidden md:mx-3 md:block w-14 bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-black)]/5  dark:[--pattern-fg:var(--color-white)]/10">
+      class="border-l border-solid border-color hidden  md:block w-14 bg-[image:repeating-linear-gradient(315deg,var(--pattern-color-light)_0,var(--pattern-color-light)_1px,transparent_0,transparent_50%)] dark:bg-[image:repeating-linear-gradient(315deg,var(--pattern-color-dark)_0,var(--pattern-color-dark)_1px,transparent_0,transparent_50%)] bg-[size:10px_10px] bg-fixed">
     </div>
     <div class="hidden lg:block"></div>
   </div>
@@ -32,5 +35,4 @@ const count = ref(0)
         <h1 class=" text-3xl font-bold underline bg-amber-900">Hello world!</h1>
       </div>
     </div> -->
-
 </template>
