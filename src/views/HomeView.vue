0cfc8f3572df21a3366caebd2a9f04f6a28007f8
Andras E <rohadtsajt@gmail.com>
2025-05-17 09:42:29 +0100

    logo in span in homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,8 @@
 import Container from "@/components/Container.vue";
 import applying from "@/assets/animations/applying.json"
 import { Vue3Lottie } from "vue3-lottie";
+import { Icon } from "@iconify/vue";
+
 </script>
 
 
@@ -17,14 +19,18 @@ import { Vue3Lottie } from "vue3-lottie";
       <!-- lottie and app-name -->
       <div class="w-3/4 lg:w-1/2 xl:w-1/3 flex flex-col items-center p-1 ">
         <Vue3Lottie :animationData="applying" :loop="true" :autoplay="true" :speed="0.6" />
-        <h1 class="text-3xl text-center">Applied</h1>
+        <h1 class="text-2xl text-center flex items-center justify-center">
+          <Icon icon="material-symbols-light:font-download-outline-rounded"
+            class="relative top-0.5 text-5xl dark:text-green-300 text-green-400 hover:rotate-90 ease-in-out duration-300" />
+          <span class="relative right-1 underline decoration-green-400 dark:decoration-green-300">pplied</span>
+        </h1>
       </div>
 
 
 
       <!-- later.. -->
 
-      <div class="flex flex-col items-center text-2xl font-bold text-center rounded-lg ">
+      <div class="flex flex-col items-center text-2xl text-center rounded-lg ">
         <div class="flex flex-center h-60 w-150 border-t border-x  border-color">
           stats
         </div>
