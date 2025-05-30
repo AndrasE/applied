8a13c06f679c19df4e84b51cd79ab5490adec8e1
AndrasE <rohadtsajt@gmail.com>
2025-05-30 16:17:31 +0100

    netlify.toml redirects

--- src/views/AddView.vue
+++ src/views/AddView.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
-import ActionButton from "@/components/ActionButton.vue";
+import { ref } from "vue";
 import Container from "@/components/Container.vue";
 import JobCard from "@/components/JobCard.vue";
 import RouterButton from "@/components/RouterButton.vue";
 
-const toggleLimit = () => {
-  // Logic to toggle limit can be added here
-  console.log("Toggle Limit button clicked");
-};
+interface Job {
+  id: number;
+  title: string;
+  description: string;
+  company: string;
+  link: string;
+}
+
+const job = ref<Job>({
+  id: 0,
+  title: "",
+  description: "",
+  company: "",
+  link: "",
+});
 </script>
 
 <template>
@@ -15,7 +26,7 @@ const toggleLimit = () => {
     <main
       class="w-full text-green-700 border-solid bg-black-900 border-y-1 border-color">
       <h1 class="text-3xl font-bold">Add</h1>
-      <JobCard viewingMode="adding" />
+      <JobCard viewingMode="adding" :key="job.id" :job="job" />
 
       <RouterButton
         to="/jobs"
@@ -24,15 +35,7 @@ const toggleLimit = () => {
         iconPosition="right"
         customClass="my-10" />
 
-      <ActionButton
-        label="Toggle Limit"
-        icon="heroicons-solid:arrow-down"
-        @click="toggleLimit"
-        customClass="my-10" />
-
-      <ActionButton outlineBtn label="Update" customClass="my-10" />
-
-      <ActionButton outlineBtn deleteBtn label="Delete" @="toggleLimit" />
+      <RouterButton label="Toggle Limit" icon="heroicons-solid:arrow-down" />
     </main>
   </Container>
 </template>
