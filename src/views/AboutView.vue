9e45985fc4ee3e3d902249cd13d48be493cf1e20
Andras E <rohadtsajt@gmail.com>
2025-06-10 06:58:02 +0100

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,6 +3,8 @@ import Container from "@/components/Container.vue";
 import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
 import { reactive, ref } from "vue";
 import type { Job } from "@/types/job";
+import { Icon } from "@iconify/vue";
+
 const props = defineProps<{
   job: Job;
   charLimit?: number;
@@ -32,16 +34,9 @@ const jobStatusOptions = ref([
         <MenuButton
           class="inline-flex items-center justify-between w-full p-1 rounded border border-color bg-[var(--bg-light)] dark:bg-[var(--bg-dark)] text-gray-700 dark:text-gray-300">
           <span>{{ editableJob.status || "Select job status" }}</span>
-          <svg
-            class="-mr-1 ml-2 h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true">
-            <path
-              fill-rule="evenodd"
-              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-              clip-rule="evenodd" />
-          </svg>
+          <Icon
+            icon="mdi:chevron-down"
+            class="w-4 h-4 ml-2 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300" />
         </MenuButton>
       </div>
 
