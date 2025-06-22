5c7e8a6389141b754a79334cbbe4bd0d4cd3d724
AndrasE <rohadtsajt@gmail.com>
2025-06-22 07:37:02 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  plugins: {
-    tailwindcss: {}, // This tells PostCSS to use the Tailwind CSS plugin
-    autoprefixer: {}, // Autoprefixer is highly recommended for cross-browser compatibility
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,css}"],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 };
