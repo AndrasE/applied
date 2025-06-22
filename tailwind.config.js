b55bd27e8d85838222a5898ba214cd8d889eac67
AndrasE <rohadtsajt@gmail.com>
2025-06-22 07:42:02 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,css}"],
+  content: [
+    "./index.html", // Your main HTML file
+    "./src/**/*.{vue,js,ts,jsx,tsx,css}", // IMPORTANT: This should cover all your source files
+  ],
   theme: {
     extend: {},
   },
