17191a1e2018cdff31f294e98affaf9819a52906
AndrasE <rohadtsajt@gmail.com>
2025-06-22 07:33:42 +0100

    test 2 tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  // The 'content' array tells Tailwind which files to scan for classes.
-  // It's crucial for Tailwind's JIT mode to generate the correct CSS.
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,css}"],
-  theme: {
-    extend: {},
+  plugins: {
+    tailwindcss: {}, // This tells PostCSS to use the Tailwind CSS plugin
+    autoprefixer: {}, // Autoprefixer is highly recommended for cross-browser compatibility
   },
-  plugins: [],
 };
