2e0b5c0229f209e963d610a02423409857d4837b
AndrasE <rohadtsajt@gmail.com>
2025-06-24 09:15:51 +0100

    toast final

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 // src/main.ts
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+
 import App from "./App.vue";
-import router from "./router";
-import "./assets/styles.css";
 import Toast from "vue-toastification";
+import router from "./router";
+// CSS
 import "vue-toastification/dist/index.css";
+import "./assets/styles.css";
+import "./assets/toast-custom.css";
 
 const app = createApp(App);
 const pinia = createPinia();
